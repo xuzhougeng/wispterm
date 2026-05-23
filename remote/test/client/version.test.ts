758d90fa846ab7caf9a28f97072a7560bc4f44d6
xuzhougeng <xuzhougeng@yeah.net>
2026-05-23 23:09:32 +0800

    Release v0.28.0

--- remote/test/client/version.test.ts
+++ remote/test/client/version.test.ts
@@ -4,14 +4,14 @@ import assert from "node:assert/strict";
 import { WEB_VERSION, remoteBrandMarkup, webVersionLabel } from "../../src/client/version";
 
 test("web version is exposed for the remote UI", () => {
-  assert.equal(WEB_VERSION, "v0.27.2");
-  assert.equal(webVersionLabel(), "Web v0.27.2");
+  assert.equal(WEB_VERSION, "v0.28.0");
+  assert.equal(webVersionLabel(), "Web v0.28.0");
 });
 
 test("web version label always includes the release version", () => {
-  assert.equal(webVersionLabel("build fixture"), "Web v0.27.2 (build fixture)");
+  assert.equal(webVersionLabel("build fixture"), "Web v0.28.0 (build fixture)");
 });
 
 test("remote brand markup exposes the web version for shell views", () => {
-  assert.equal(remoteBrandMarkup(), 'Phantty Remote <span class="web-version">Web v0.27.2</span>');
+  assert.equal(remoteBrandMarkup(), 'Phantty Remote <span class="web-version">Web v0.28.0</span>');
 });
